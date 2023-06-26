388b1fc748431a18b6ed09b983fff50a9825d860
MuertoParado <nicolas.casanova20@gmail.com>
2023-06-25 23:12:11 -0500

    feat: aaa

--- routes/api.php
+++ routes/api.php
@@ -8,20 +8,31 @@ $dotenv = Dotenv::createImmutable('./config/env/'); // -> config de el enviromen
 $dotenv->load();
 $routes = new \Bramus\Router\Router();
 
-$routes->mount('/workrefence/', function() use ($routes) {
+$routes->mount('/workrefence', function() use ($routes) {
+    echo"wr";
     $routes->get('/', 'app\Controllers\work_reference_Controller@GetWorkReference');
     $routes->post('/add', 'app\Controllers\work_reference_Controller@PostWorkReferences');
     $routes->delete('/delete', 'app\Controllers\work_reference_Controller@DeleteWorkReferences');
     
 });
 
-$routes->mount('/personalref/', function() use ($routes) {
+$routes->mount('/personalref', function() use ($routes) {
     $routes->get('/', 'app\Controllers\personal_ref_Controller@GetPersonalRef');
     $routes->post('/add', 'app\Controllers\personal_ref_Controller@PostPersonalRef');
     $routes->put('/{id}', 'app\Controllers\personal_ref_Controller@PutPersonalRef');
     $routes->delete('/{id}', 'app\Controllers\personal_ref_Controller@DeletePersonalRef');
 
 });
+
+$routes->mount('/workinginfo', function() use ($routes) {
+    $routes->get('/', 'app\Controllers\personal_ref_Controller@GetPersonalRef');
+    $routes->post('/add', 'app\Controllers\personal_ref_Controller@PostPersonalRef');
+    $routes->put('/{id}', 'app\Controllers\personal_ref_Controller@PutPersonalRef');
+    $routes->delete('/{id}', 'app\Controllers\personal_ref_Controller@DeletePersonalRef');
+
+});
+
+
 $routes->post ('/', function(){
     echo "b";
 });
